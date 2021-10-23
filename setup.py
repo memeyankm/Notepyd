e75c68c9080ea2d63152b1ce31cc8597d2da3b41
Meyank M <65050847+memeyankm@users.noreply.github.com>
2021-10-23 22:03:39 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dNotepyd',
+      version='1.0',
+      description='Notepad clone made using python',
+      packages=['Notepyd'],
+      zip_safe=False)
